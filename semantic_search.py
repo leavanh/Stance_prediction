882d41fecb5e148dcb2ec22ec0466ce38c790c0f
Lea Schulz-Vanheyden <lea.schulz-vanheyden@gmx.de>
2022-11-07 14:42:12 +0100

    test with grüne manifesto

--- semantic_search.py
+++ semantic_search.py
@@ -1,8 +1,17 @@
+import pandas as pd
+import numpy as np
+import torch
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
 
-query_embedding = model.encode('How big is London')
-passage_embedding = model.encode(['London has 9,787,426 inhabitants at the 2011 census',
-                                  'London is known for its finacial district'])
+manifesto = pd.read_excel(r'/Users/lea_vanheyden/Library/Mobile Documents/com~apple~CloudDocs/Documents/Uni/Masterarbeit/Polit Language Model/Data/Wahlprogramme/grüne 2021.xlsx')
+print(manifesto)
+manifesto_sentences = manifesto['text']
 
-print("Similarity:", util.dot_score(query_embedding, passage_embedding))
+query_embedding = model.encode('öffentlicher Nahverkehr sollte kostenlos sein')
+passage_embedding = model.encode(manifesto_sentences)
+
+similarity = util.dot_score(query_embedding, passage_embedding)
+value, index = torch.topk(similarity, 10)
+with pd.option_context('display.max_colwidth', None):
+    print(manifesto_sentences.iloc[index[0]])
