d1f63b74fc65c82c9377c1d2458bfd4b1785051f
Lea Schulz-Vanheyden <lea@schulz-vanheyden.de>
2022-12-02 17:13:40 +0000

    test whitening on Grüne

--- sBERT_whitening.py
+++ sBERT_whitening.py
@@ -1,6 +1,6 @@
 import torch
 import tensorflow as tf
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
 import pandas as pd
 import numpy as np
 from semantic_search import get_sent_list #ToDo: fix, not working yet
@@ -16,13 +16,17 @@ query_embedding = model.encode(query)
 grüne_embedding = model.encode(grüne_tok)
 grüne_embedding.shape
 
-embeddings = grüne_embedding
 def whitening_torch(embeddings): #check if does the same as with tensors, check dims
     mu = np.mean(embeddings, axis=0, keepdims=True)
     cov = np.matmul(np.transpose((embeddings - mu)), embeddings - mu)
     u, s, vh = np.linalg.svd(cov)
     W = np.matmul(u, np.diag(1/np.sqrt(s)))
     embeddings = np.matmul(embeddings - mu, W)
-    return embeddings
+    return embeddings, mu, W
 
-whitening_torch(grüne_embedding)
+grüne_white, mu, W = whitening_torch(grüne_embedding)
+query_white = np.matmul(query_embedding - mu, W)
+
+similarity = util.dot_score(query_white, grüne_white)
+value, index = torch.topk(similarity, 5)
+grüne_sim = [grüne_tok[i] for i in index.tolist()[0]]
