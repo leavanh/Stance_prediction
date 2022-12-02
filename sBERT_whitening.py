2f6c5410983ff783601e2f3c5541a41c4c9fdeb7
Lea Schulz-Vanheyden <lea@schulz-vanheyden.de>
2022-12-02 17:01:27 +0000

    transform whitening algorithm from torch to numpy

--- sBERT_whitening.py
+++ sBERT_whitening.py
@@ -1,1 +1,28 @@
 import torch
+import tensorflow as tf
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import numpy as np
+from semantic_search import get_sent_list #ToDo: fix, not working yet
+
+query = "Elternunabhängiges Bafög für alle Studierenden"
+
+grüne = pd.read_excel(r'stance_prediction/Stance_prediction/Wahlprogramme/grüne 2021.xlsx')['text']
+grüne_tok = get_sent_list(grüne)
+
+model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
+
+query_embedding = model.encode(query)
+grüne_embedding = model.encode(grüne_tok)
+grüne_embedding.shape
+
+embeddings = grüne_embedding
+def whitening_torch(embeddings): #check if does the same as with tensors, check dims
+    mu = np.mean(embeddings, axis=0, keepdims=True)
+    cov = np.matmul(np.transpose((embeddings - mu)), embeddings - mu)
+    u, s, vh = np.linalg.svd(cov)
+    W = np.matmul(u, np.diag(1/np.sqrt(s)))
+    embeddings = np.matmul(embeddings - mu, W)
+    return embeddings
+
+whitening_torch(grüne_embedding)
