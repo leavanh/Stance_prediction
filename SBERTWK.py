6e37ae3c266ad9af2ecacb54644e19d42c0963a9
Lea Schulz-Vanheyden <lea@schulz-vanheyden.de>
2022-12-30 13:35:54 +0000

    SBERWK batching works

--- SBERTWK.py
+++ SBERTWK.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import, division, unicode_literals
 import sys
 import io
 import numpy as np
+import pandas as pd
 import logging as lg
 import argparse
 import torch
+from torch.utils.data import Dataset, DataLoader
 import random
 
 from transformers import AutoConfig, AutoTokenizer, AutoModelWithLMHead
@@ -20,12 +22,37 @@ def set_seed(args):
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
+# -----------------------------------------------
+def trun_pad(sentences):
+    sentences_index = [tokenizer.encode(s, add_special_tokens=True) for s in sentences]
+    features_input_ids = []
+    features_mask = []
+    for sent_ids in sentences_index:
+        # Truncate if too long
+        if len(sent_ids) > params["max_seq_length"]:
+            sent_ids = sent_ids[: params["max_seq_length"]]
+        sent_mask = [1] * len(sent_ids)
+        # Padding
+        padding_length = params["max_seq_length"] - len(sent_ids)
+        sent_ids += [0] * padding_length
+        sent_mask += [0] * padding_length
+        # Length Check
+        assert len(sent_ids) == params["max_seq_length"]
+        assert len(sent_mask) == params["max_seq_length"]
+
+        features_input_ids.append(sent_ids)
+        features_mask.append(sent_mask)
+    features_mask = np.array(features_mask)
+    return features_input_ids, features_mask
 
 # -----------------------------------------------
 # Settings
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--batch_size", default=64, type=int, help="batch size for extracting features."
+)
+parser.add_argument(
+    "--num_epochs", default=2, type=int, help="number of epochs."
 )
 parser.add_argument(
     "--max_seq_length",
@@ -85,62 +112,55 @@ tokenizer = AutoTokenizer.from_pretrained(params["model_type"], cache_dir="./cac
 model = AutoModelWithLMHead.from_pretrained(
     params["model_type"], config=config, cache_dir="./cache"
 )
+
 model.to(params["device"])
 
-# -----------------------------------------------
-
-sentences = grüne_tok
-random.shuffle(sentences)
 
 # -----------------------------------------------
-sentences_index = [tokenizer.encode(s, add_special_tokens=True) for s in sentences]
-features_input_ids = []
-features_mask = []
-for sent_ids in sentences_index:
-    # Truncate if too long
-    if len(sent_ids) > params["max_seq_length"]:
-        sent_ids = sent_ids[: params["max_seq_length"]]
-    sent_mask = [1] * len(sent_ids)
-    # Padding
-    padding_length = params["max_seq_length"] - len(sent_ids)
-    sent_ids += [0] * padding_length
-    sent_mask += [0] * padding_length
-    # Length Check
-    assert len(sent_ids) == params["max_seq_length"]
-    assert len(sent_mask) == params["max_seq_length"]
+# Load data
+class Manifestos(Dataset):
+    def __init__(self):
+        # data loading
+        self.sentences = list(pd.read_excel(r'/home/ubuntu/thesis/stance_prediction/Stance_prediction/Wahlprogramme/alle_tokenized.xlsx')[0])
+        random.shuffle(self.sentences)
+        self.n_samples = len(self.sentences)
 
-    features_input_ids.append(sent_ids)
-    features_mask.append(sent_mask)
+    def __getitem__(self, index):
+        return self.sentences[index]
 
-features_mask = np.array(features_mask)
+    def __len__(self):
+        return self.n_samples
 
-# create and go through the batches
-pos = 0
-while pos<len(sentences):
-    # `to_take` is our actual batch size. It will be `batch_size` until 
-    # we get to the last batch, which may be smaller. 
-    to_take = min(params["batch_size"], len(sentences)-pos)
-    batch_input_ids = torch.tensor(features_input_ids[pos:(pos+to_take)], dtype=torch.long)
-    batch_input_mask = torch.tensor(features_mask[pos:(pos+to_take)], dtype=torch.long)
-    batch = [batch_input_ids.to(device), batch_input_mask.to(device)]
+dataset = Manifestos()
+dataloader = DataLoader(dataset=dataset, batch_size=params["batch_size"], shuffle=False, num_workers=2)
 
-    inputs = {"input_ids": batch[0], "attention_mask": batch[1]}
-    model.zero_grad()
+# -----------------------------------------------
+# Train model
+for epoch in range(params["num_epochs"]):
+    embeddings = []
+    for i, sentences in enumerate(dataloader):
 
-    with torch.no_grad(): #TODO fix this problem
-        features = model(**inputs)[1]
+        features_input_ids, features_mask = trun_pad(sentences)
 
-    pos = (pos + to_take)-1
+        batch_input_ids = torch.tensor(features_input_ids, dtype=torch.long)
+        batch_input_mask = torch.tensor(features_mask, dtype=torch.long)
+        batch = [batch_input_ids.to(device), batch_input_mask.to(device)]
 
+        inputs = {"input_ids": batch[0], "attention_mask": batch[1]}
+        model.zero_grad()
 
+        with torch.no_grad():
+            features = model(**inputs)[1]
 
-# Reshape features from list of (batch_size, seq_len, hidden_dim) for each hidden state to list
-# of (num_hidden_states, seq_len, hidden_dim) for each element in the batch.
-all_layer_embedding = torch.stack(features).permute(1, 0, 2, 3).cpu().numpy()
+        # Reshape features from list of (batch_size, seq_len, hidden_dim) for each hidden state to list
+        # of (num_hidden_states, seq_len, hidden_dim) for each element in the batch.
+        all_layer_embedding = torch.stack(features).permute(1, 0, 2, 3).cpu().numpy()
+        embed_method = SBERTWK_utils.generate_embedding(params["embed_method"], features_mask)
+        embedding = embed_method.embed(params, all_layer_embedding)
+        embeddings.extend(list(embedding))
 
-embed_method = SBERTWK_utils.generate_embedding(params["embed_method"], features_mask)
-embedding = embed_method.embed(params, all_layer_embedding)
-
+# -----------------------------------------------
+# Compute similarity
 similarity = (
     embedding[0].dot(embedding[1])
     / np.linalg.norm(embedding[0])
